6cce11e793e190c0ddfd1ff8940f1576315704fa
Ocece77 <oceanekasindupro@gmail.com>
2024-05-14 21:25:41 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Définir la base sur /portfolio/
   build: {
-    
     rollupOptions: {
       onwarn(warning, warn) {
         if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
