a011ccbddd7a58b989a90afef85d6c30d22200e4
Ocece77 <oceanekasindupro@gmail.com>
2024-05-14 21:06:33 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [react()],
-})
+});
