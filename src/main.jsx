12a57309e97bb99d4b6f0a1d3de4225a893278dd
Ocece77 <oceanekasindupro@gmail.com>
2024-05-14 20:42:27 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
