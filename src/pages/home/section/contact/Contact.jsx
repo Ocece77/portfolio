77f47068bb0ad0b2c91e9ce00345a395e97d927c
Ocece77 <oceanekasindupro@gmail.com>
2024-05-17 01:13:30 +0200

    correctin some bug :)

--- src/pages/home/section/contact/Contact.jsx
+++ src/pages/home/section/contact/Contact.jsx
@@ -15,12 +15,13 @@ return (
         <h1 className="col-lg-6 col-md-6 col-sm-12 fs-1 fw-bolder text-center text-uppercase my-5 ">J'ai hâte de travailler avec vous pour accomplir vos objectifs !</h1>
 
           <Container className="col-12 col-lg-3 col-md-5  d-flex  justify-content-center align-items-center "  >
-                <img src={qrcode} alt="qrcode linkedin"  className="col-lg-5 col-sm-3 col-md-5 img img-fluid"/>
+                <img src={qrcode} alt="qrcode linkedin"  className="col-3 col-lg-5 col-md-5 col-sm-3  img img-fluid"/>
   
            <ul style={{ listStyleType: 'none' }}>
               <li><a  className="fs-4  text-decoration-none text-dark" href="mailto:oceanekasindupro@gmail.com"  rel="noopener noreferrer">Mail</a></li>
               <li><a   className="fs-4  text-decoration-none text-dark" href="https://www.linkedin.com/in/oceanekasindu/"  rel="noopener noreferrer">Linkedin</a></li>
               <li><a   className="fs-4  text-decoration-none text-dark" href="https://github.com/Ocece77"  rel="noopener noreferrer">Github</a></li>
+              <li><a   className="fs-4  text-decoration-none text-dark" href="https://leetcode.com/u/Ocece77/"  rel="noopener noreferrer">LeetCode</a></li>
             </ul>
         
 
