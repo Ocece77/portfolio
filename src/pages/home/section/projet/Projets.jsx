860a17dda8852bcc3fa1f34825e86301ec47100e
Ocece77 <oceanekasindupro@gmail.com>
2024-07-02 12:26:33 +0200

    add new project

--- src/pages/home/section/projet/Projets.jsx
+++ src/pages/home/section/projet/Projets.jsx
@@ -5,6 +5,7 @@ import frontend from './img/frontend.png';
 import webprojet from './img/webprojet.png';
 import python from './img/python.png';
 import cv_generator from './img/cv_generator.png';
+import messagerie from './img/messagerie.png';
 import Box from "./Box";
 
 const Projets = () => {
@@ -18,10 +19,12 @@ const Projets = () => {
             <p className="text-light">Ma passion pour la programmation me pousse constamment à m'améliorer. En dehors de mes heures de cours, <span class='text-decoration-underline'>je consacre mon temps à la réalisation de divers projets</span>, explorant ainsi une multitude de technologies. Parallèlement à ma formation, j'ai également eu l'occasion de concrétiser plusieurs projets, dont voici quelques exemples concrets.</p>
           </Container>
 
-          <Box lg="5" src={frontend} name="Frontend mentor" desc='Regroupement de mes projets web' url='https://github.com/Ocece77/frontend-mentor'/>
+          <Box lg="5" src={frontend} name="Frontend mentor" desc='Regroupement de mes projets web' url='https://ocece77.github.io/frontend-mentor/'/>
           <Box lg="5" src={python} name="Python"  desc='Projets en python !' url='https://github.com/Ocece77/python-project'/>
-          <Box lg="5" src={webprojet} name="Projet API"  desc='Regroupement de mes projets web utilisant principalement des API' url='https://github.com/Ocece77/Web-Project'/>
-          <Box lg="11" src={cv_generator} name="Createur de CV"  desc='Réalisé avec REACT TS et boostrap, ce projet est un créateur avec un template prédéfini' url='https://github.com/Ocece77/cv_creation_app'/>
+          <Box lg="5" src={webprojet} name="Projet API"  desc='Regroupement de mes projets web utilisant principalement des API' url='https://ocece77.github.io/Web-Project/'/>
+          <Box lg="5" src={cv_generator} name="Createur de CV"  desc='Réalisé avec REACT TS et boostrap, ce projet est un créateur avec un template prédéfini' url='https://ocece77.github.io/cv_creation_app/'/>
+          <Box lg="5" src={messagerie} name="Messagerie web "  desc='Postez des messages sur cette mini app web de messagerie' url='https://message-live-app-1.onrender.com'/>
+
           
         </Container>
       </Container>
